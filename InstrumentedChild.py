fce7c3e2042dd6b80a82230afba9404de687ab53
Paul Feitzinger <paul@pfeyz.com>
2020-11-22 18:28:27 -0500

    fix trigger pre-cacheing

--- InstrumentedChild.py
+++ InstrumentedChild.py
@@ -1,23 +1,91 @@
+import re
 from NDChild import NDChild
 
-def set_trigger(sentence, param, direction, conservative: bool):
-    sentence.triggers[param] = (direction, conservative)
+class TriggerCacher:
+    def __init__(self):
+        self.current_trigger = None
+        self.current_sentence = None
+        self.trigger_methods = [NDChild.spEtrigger,
+                                NDChild.hipEtrigger,
+                                NDChild.hcpEtrigger,
+                                NDChild.optEtrigger,
+                                NDChild.nsEtrigger,
+                                NDChild.ntEtrigger,
+                                NDChild.whmEtrigger,
+                                NDChild.piEtrigger,
+                                NDChild.tmEtrigger,
+                                NDChild.VtoIEtrigger,
+                                NDChild.ItoCEtrigger,
+                                NDChild.ahEtrigger,
+                                NDChild.QInvEtrigger]
 
+    def trigger_name(self, method):
+        name = re.sub('Etrigger', '', method.__name__)
+        name = name.upper() if name.islower() else name
+        return name
+
+    def set_trigger(self, source_trigger, sentence, param, direction, conservative: bool):
+        trigs = sentence.triggers
+        if source_trigger not in trigs:
+            trigs[source_trigger] = [(param, direction, conservative)]
+        else:
+            trigs[source_trigger].append((param, direction, conservative))
+
+    def adjustweight(self, param, direction):
+        self.set_trigger(self.current_trigger, self.current_sentence, param, direction, False)
+
+    def adjustweightConservatively(self, param, direction):
+        self.set_trigger(self.current_trigger, self.current_sentence, param, direction, True)
+
+    def consume_sentence(self, s):
+        child = NDChild(None, None, None)
+        child.adjustweight = self.adjustweight
+        child.adjustweightConservatively = self.adjustweightConservatively
+        for trigger in self.trigger_methods:
+            self.current_trigger = self.trigger_name(trigger)
+            self.current_sentence = s
+            trigger(child, s)
+        self.current_trigger = None
+        self.current_sentence= None
+
+cacher = TriggerCacher()
 
 class InstrumentedNDChild(NDChild):
-
     @staticmethod
     def precompute_sentence(sentence):
+        cacher.consume_sentence(sentence)
+        return
         child = NDChild(None, None, 611)
+        current_trigger = [None]
         child.adjustweight = lambda param, direction: (
-            set_trigger(sentence, param, direction, conservative=False))
+            set_trigger(current_trigger[0], sentence, param, direction, conservative=False))
         child.adjustweightConservatively = lambda param, direction: (
-            set_trigger(sentence, param, direction, conservative=True))
-        child.consumeSentence(sentence)
+            set_trigger(current_trigger[0], sentence, param, direction, conservative=True))
 
-    def handleTrigger(self, sentence, param):
-        if param in sentence.triggers:
-            direction, conservative = sentence.triggers[param]
+        triggers = [child.spEtrigger,
+                    child.hipEtrigger,
+                    child.hcpEtrigger,
+                    child.optEtrigger,
+                    child.nsEtrigger,
+                    child.ntEtrigger,
+                    child.whmEtrigger,
+                    child.piEtrigger,
+                    child.tmEtrigger,
+                    child.VtoIEtrigger,
+                    child.ItoCEtrigger,
+                    child.ahEtrigger,
+                    child.QInvEtrigger]
+
+        for trigger in triggers:
+            name = re.sub('Etrigger', '', trigger.__name__)
+            name = name.upper() if name.islower() else name
+            current_trigger[0] = name
+            trigger(sentence)
+
+    def handleTrigger(self, sentence, etrigger):
+        if etrigger not in sentence.triggers:
+            return
+        for param, direction, conservative in sentence.triggers[etrigger]:
             if conservative:
                 self.adjustweightConservatively(param, direction)
             else:
@@ -52,17 +120,3 @@ class InstrumentedNDChild(NDChild):
 
     def whmEtrigger(self, s):
         self.handleTrigger(s, 'WHM')
-
-
-# for method in ['QInvEtrigger', 'VtoIEtrigger', 'hcpEtrigger', 'hipEtrigger',
-#                'nsEtrigger', 'ntEtrigger', 'piEtrigger', 'spEtrigger',
-#                'tmEtrigger', 'whmEtrigger']:
-#     param = method.replace('Etrigger', '')
-#     if param.islower():
-#         param = param.upper()  #
-#     print("""    def {method}(self, s):
-#         if '{param}' in s.triggers:
-#             direction, rate = s.triggers['{param}']
-#             self.adjustweight('{param}', direction, rate)
-#         """.format(
-#         method=method, param=param))
